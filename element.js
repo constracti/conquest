f4f0bf77b3909235e111069f875c8d75616939f6
constracti <constracti@gmail.com>
2026-01-19 20:48:53 +0200

    use common form elements

--- element.js
+++ element.js
@@ -107,3 +107,137 @@ export function n_option_list(option_list, option_null) {
 	}
 	return element_list;
 }
+
+/**
+ * @param {object} options
+ * @param {?string} options.type - default: text
+ * @param {string} options.id
+ * @param {string} options.label
+ * @param {string} options.name
+ * @param {?string} options.min
+ * @param {?HTMLOptionElement[]} options.option_list
+ * @param {?string} options.value
+ * @param {?boolean} options.required - default: false
+ * @param {?string} options.text
+ * @returns {HTMLDivElement}
+ */
+export function n_form_control(options) {
+	if (options.type === undefined)
+		options.type = null;
+	if (options.type === null)
+		options.type = 'text';
+	if (options.min === undefined)
+		options.min = null;
+	if (options.option_list === undefined)
+		options.option_list = null;
+	if (options.value === undefined)
+		options.value = null;
+	if (options.required === undefined)
+		options.required = null;
+	if (options.required === null)
+		options.required = false;
+	if (options.text === undefined)
+		options.text = null;
+	const div = document.createElement('div');
+	div.className = 'm-1 flex-grow-1';
+	const label = document.createElement('label');
+	label.htmlFor = options.id;
+	label.className = 'mb-1 form-label';
+	const label_text = document.createElement('span');
+	label_text.innerHTML = options.label;
+	label.append(label_text);
+	if (options.required) {
+		const label_ast = document.createElement('span');
+		label_ast.className = 'text-danger';
+		label_ast.innerHTML = '*';
+		label.append(' ', label_ast);
+	}
+	div.append(label);
+	if (options.type !== 'select') {
+		const input = document.createElement('input');
+		input.type = options.type;
+		input.name = options.name;
+		if (options.min !== null)
+			input.min = options.min;
+		if (options.value !== null)
+			input.value = options.value;
+		input.required = options.required;
+		input.id = options.id;
+		input.className = 'form-control form-control-sm';
+		div.append(input);
+	} else {
+		const select = document.createElement('select');
+		select.name = options.name;
+		if (options.option_list !== null)
+			select.append(...options.option_list);
+		if (options.value !== null)
+			select.value = options.value;
+		select.required = options.required;
+		select.id = options.id;
+		select.className = 'form-select form-select-sm';
+		div.append(select);
+	}
+	if (options.text !== null) {
+		const text = document.createElement('div');
+		text.className = 'mt-1 form-text';
+		text.innerHTML = options.text;
+		div.append(text);
+	}
+	return div;
+}
+
+/**
+ * @param {string} name
+ * @param {?string} value
+ * @returns {HTMLInputElement}
+ */
+export function n_form_hidden(name, value) {
+	if (value === undefined)
+		value = null;
+	const input = document.createElement('input');
+	input.type = 'hidden';
+	input.name = name;
+	if (value !== null)
+		input.value = value;
+	return input;
+}
+
+/**
+ * @returns {HTMLDivElement}
+ */
+export function n_form_submit() {
+	const div = document.createElement('div');
+	div.className = 'm-1';
+	const label = document.createElement('label');
+	label.className = 'mb-1 form-label d-block';
+	label.innerHTML = '&nbsp;';
+	div.append(label);
+	const button = document.createElement('button');
+	button.type = 'submit';
+	button.className = 'btn btn-primary btn-sm';
+	button.innerHTML = 'Submit';
+	div.append(button);
+	return div;
+}
+
+/**
+ * @param {HTMLElement[]} element_list
+ * @returns {HTMLDivElement}
+ */
+export function n_form_cancel(element_list) {
+	const div = document.createElement('div');
+	div.className = 'm-1';
+	const label = document.createElement('label');
+	label.className = 'mb-1 form-label d-block';
+	label.innerHTML = '&nbsp;';
+	div.append(label);
+	const button = document.createElement('button');
+	button.type = 'button';
+	button.className = 'btn btn-secondary btn-sm';
+	button.innerHTML = 'Cancel';
+	button.addEventListener('click', () => {
+		element_list.forEach(element => element.classList.toggle('d-none'));
+	});
+	div.append(button);
+	return div;
+}
